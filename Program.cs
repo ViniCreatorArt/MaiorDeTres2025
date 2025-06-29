9327ded535fe642aab077cc157f0331804429b5f
Vinicius A. <viniusername@gmail.com>
2025-06-29 19:04:56 -0300

    exercicio finalizado

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("- Maior De Três -\n");
+
+Console.Write("Digite o primeiro número...: ");
+int Pn = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o segundo número....: ");
+int Sn = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o terceiro número...: ");
+int Tn = Convert.ToInt32(Console.ReadLine());
+
+int maiorNumero;
+
+if (Pn > Sn)
+{
+    if (Pn > Tn)
+    {
+        maiorNumero = Pn;
+    }
+    else
+    {
+        maiorNumero = Tn;
+    }
+}
+else
+{
+    if (Sn > Tn)
+    {
+        maiorNumero = Sn;
+    }
+    else
+    {
+        maiorNumero = Tn;
+    }
+}
+
+Console.WriteLine($"\nMaior número: {maiorNumero}");
